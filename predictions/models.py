387f9b4ae557c0b34d6634982278808428c52669
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-08-07 13:29:52 +0200

    Add dummy template for predictions view

--- predictions/models.py
+++ predictions/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Prediction(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- predictions/views.py
+++ predictions/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Prediction
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the predictions index.")
+    latest_predictions_list = Prediction.objects.order_by("-created_at")[:5]
+    template = loader.get_template("predictions/index.html")
+    context = {
+        "latest_predictions_list": latest_predictions_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, prediction_id):
     response = "You're looking at the results of prediction %s."
