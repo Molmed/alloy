61532a6fcdbab9e01fdfd4e3fd234f3b4e09eab5
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-08-07 13:11:29 +0200

    Add route to view prediction

--- predictions/views.py
+++ predictions/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the predictions index.")
+
+def detail(request, prediction_id):
+    response = "You're looking at the results of prediction %s."
+    return HttpResponse(response % prediction_id)
