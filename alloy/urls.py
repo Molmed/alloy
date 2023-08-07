5a06184bbcad875883a8e4509986f10f05ba2dde
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-08-07 12:24:16 +0200

    Add predictions app

--- alloy/urls.py
+++ alloy/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("predictions/", include("predictions.urls")),
     path('admin/', admin.site.urls),
 ]
